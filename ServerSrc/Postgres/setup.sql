54c43b6df51d80f883af0ec814c2e6bf77e86c78
Aria Diamond <jwdiamon@ucsc.edu>
2021-12-06 17:02:02 -0500

    Set up Postgres docker container

--- ServerSrc/Postgres/setup.sql
+++ ServerSrc/Postgres/setup.sql
@@ -1,25 +1,25 @@
 /* Sets up PostgreSQL database with the tables we need for the Cryptik server to interact with. */
 
 CREATE TABLE Users (
-	id     CHAR(4) PRIMARY KEY,
+	id     CHAR(4)  PRIMARY KEY,
 	pubKey CHAR(48) NOT NULL -- 32 bytes of data in base64
 );
 
 CREATE TABLE Channels (
-	channel INTEGER PRIMARY KEY,
-	members VARCHAR() NOT NULL, -- TODO how many max people in a chat?
-	next    CHAR(4)   REFERENCES Users(id), -- this can be NULL if the channel has been initialized
-	g       CHAR(192)    NOT NULL, -- base64 of 2048
-	p       CHAR(192)    NOT NULL, -- base64 of 2048
-	exps    VARCHAR()
+	channel   INTEGER      PRIMARY KEY,
+	members   VARCHAR(50)  NOT NULL,
+	next      CHAR(4)      REFERENCES Users(id), -- this can be NULL if the channel is ready
+	g         BIT(2048)    NOT NULL,
+	p         BIT(2048)    NOT NULL,
+	exps      VARCHAR(500),
+    signature CHAR(128) -- signature size is 64 bytes, but 128 in hex
 );
 
 -- Create a sequence to do autoincrement to create a unique key for each message (as required by
 -- SQL). This assumes no more than 2^31 messages will be sent.
 -- https://www.postgresql.org/docs/14/datatype-numeric.html
-CREATE SEQUENCE Message_Autoinc_seq AS INTEGER;
 CREATE TABLE Messages (
-	Autoinc SERIAL4   PRIMARY KEY DEFAULT nextval('Message_Autoinc_seq'),
+	Autoinc SERIAL4   PRIMARY KEY,
 	channel INTEGER   NOT NULL    REFERENCES Channels(channel),
-	message VARCHAR() NOT NULL,
+	message VARCHAR(2048) NOT NULL
 );
