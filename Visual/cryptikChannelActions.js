a1a8417a1083606b97b96bba0491e123a65f9803
Aria Diamond <jwdiamon@ucsc.edu>
2022-06-17 21:14:51 -0700

    Remove Visual folder

--- Visual/cryptikChannelActions.js
+++
@@ -1,7 +0,0 @@
-const message = document.querySelectorAll(".message");
-let delay = 0;
-message.forEach(el=>{
-  el.style.animation = "fade-in 1s ease forwards";
-  el.style.animationDelay= delay +"s";
-  delay += 0.33;
-});
